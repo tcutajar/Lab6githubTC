eefd2b76ccc78da851b7bc359b73bc77e9d99ccc
tcutajar <tcutajar@ufl.edu>
2023-10-23 16:46:24 -0400

    Lab 6 addition Tyler Cutajar

--- version_control.py
+++ version_control.py
@@ -1,6 +1,5 @@
-def main(): # Tyler Cutajar
+def main():  # Tyler Cutajar
     menu = True
-
     while menu:
         print(f"\nMenu\n"
               "-------------")
@@ -10,10 +9,9 @@ def main(): # Tyler Cutajar
 
         option = input("\nPlease enter an option: ")
 
-
         if option == "1":
             password = input("Please enter your password to encode: ")
-            encode(password)
+            en_password = encode(password)
             print("Your password has been encoded and stored!")
         elif option == "2":
             pass
@@ -22,7 +20,7 @@ def main(): # Tyler Cutajar
 
 
 def encode(passwrd):
-    temp_pswrd= ""
+    temp_pswrd = ""
     for i in range(0, len(passwrd)):
         if int(passwrd[i]) >= 7:
             temp_pswrd += str(int(passwrd[i]) + 3)[1:2]
@@ -30,6 +28,7 @@ def encode(passwrd):
         else:
             temp_pswrd += str(int(passwrd[i]) + 3)
     return temp_pswrd
+
 
 
 if __name__ == '__main__':
